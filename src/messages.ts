41314506e4f5750928e87719a9fcd821fb27ff98
dboikliev <deyanpb@hotmail.com>
2016-09-10 14:56:02 +0300

    Adds KeepAlive, Handshake, Piece, Cancel messages.

--- src/messages.ts
+++ src/messages.ts
@@ -8,6 +8,75 @@ export namespace Messages {
         data: ArrayBuffer;
     }
 
+    export class KeepAlife implements IMessage {
+        get length(): number {
+            return 1;
+        }
+
+        get messageId(): number {
+            return -2;
+        }
+
+        get payload(): ArrayBuffer {
+            return null;
+        }
+
+        get data(): ArrayBuffer {
+            return new Uint8Array([ 0, 0, 0, 0]).buffer;
+        }
+    }
+
+    export class Handshake implements IMessage {
+        private static message: string = "BitTorrent protocol";
+        private static clientId: string = "-DB1000-012345678901";
+        private static options: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
+        private infoHash: string;
+
+        constructor(infoHash: string) {
+            this.infoHash = infoHash;
+        }
+
+        get length(): number {
+            let combinedLength = 1 + Handshake.message.length + Handshake.clientId.length + Handshake.options.length + this.infoHash.length;
+            return combinedLength;
+        }
+
+        get messageId(): number {
+            return -1;
+        }
+
+        get payload(): ArrayBuffer {
+            let arr = [];
+            this.infoHash.match(/.{2}/g).forEach(element => {
+                arr.push(parseInt(element, 16));
+            });
+
+            for (let i = 0; i < Handshake.clientId.length; i++) {
+                arr.push(Handshake.clientId.charCodeAt(i));
+            }
+
+            let view = new Uint8Array(arr);
+            return  view.buffer;
+        }
+
+        get data(): ArrayBuffer {
+            let arr: number[] = [ Handshake.message.length ];
+
+            for (let i = 0; i < Handshake.message.length; i++) {
+                arr.push(Handshake.message.charCodeAt(i));
+            }
+
+            for (let index in Handshake.options) {
+                arr.push(Handshake.options[index]);
+            }
+
+            let messageHeader = new Uint8Array(arr);
+            let messageBody = new Uint8Array(this.payload);
+            let fullMessage = BinaryOperations.ByteConverter.combineByteArrays(messageHeader, messageBody);
+            return fullMessage;
+        }
+    }
+
     export class Choke implements IMessage {
         get length(): number {
             return 1;
@@ -179,11 +248,73 @@ export namespace Messages {
         }
     }
 
-    // export class Piece implements IMessage {
+    export class Piece implements IMessage {
+        private pieceIndex: number;
+        private begin: number;
+        private pieceLength: number;
 
-    // }
+        constructor(pieceIndex: number, begin: number, pieceLength: number) {
+            this.pieceIndex = pieceIndex;
+            this.begin = begin;
+            this.pieceLength = pieceLength;
+        }
 
-    // export class Cancel implements IMessage {
+        get length(): number {
+            return 13;
+        }
 
-    // }
+        get messageId(): number {
+            return 6;
+        }
+
+        get payload(): ArrayBuffer {
+            let pieceIndexBytes = BinaryOperations.ByteConverter.convertUint32ToUint8Array(this.pieceIndex, 4);
+            let beginBytes = BinaryOperations.ByteConverter.convertUint32ToUint8Array(this.begin, 4);
+            let pieceLengthBytes = BinaryOperations.ByteConverter.convertUint32ToUint8Array(this.pieceLength, 4);
+            let fullMessage = BinaryOperations.ByteConverter.combineByteArrays(pieceIndexBytes, beginBytes, pieceLengthBytes);
+            return fullMessage.buffer;
+        }
+
+        get data(): ArrayBuffer {
+            let messageHeader =  new Uint8Array([ 0, 0, 0, this.pieceLength, this.messageId ]);
+            let messageBody = new Uint8Array(this.payload);
+            let fullMessage = BinaryOperations.ByteConverter.combineByteArrays(messageHeader, messageBody);
+            return fullMessage.buffer;
+        }
+    }
+
+    export class Cancel implements IMessage {
+        private pieceIndex: number;
+        private begin: number;
+        private pieceLength: number;
+
+        constructor(pieceIndex: number, begin: number, pieceLength: number) {
+            this.pieceIndex = pieceIndex;
+            this.begin = begin;
+            this.pieceLength = pieceLength;
+        }
+
+        get length(): number {
+            return 13;
+        }
+
+        get messageId(): number {
+            return 8;
+        }
+
+        get payload(): ArrayBuffer {
+            let pieceIndexBytes = BinaryOperations.ByteConverter.convertUint32ToUint8Array(this.pieceIndex, 4);
+            let beginBytes = BinaryOperations.ByteConverter.convertUint32ToUint8Array(this.begin, 4);
+            let pieceLengthBytes = BinaryOperations.ByteConverter.convertUint32ToUint8Array(this.pieceLength, 4);
+            let fullMessage = BinaryOperations.ByteConverter.combineByteArrays(pieceIndexBytes, beginBytes, pieceLengthBytes);
+            return fullMessage.buffer;
+        }
+
+        get data(): ArrayBuffer {
+            let messageHeader =  new Uint8Array([ 0, 0, 0, this.pieceLength, this.messageId ]);
+            let messageBody = new Uint8Array(this.payload);
+            let fullMessage = BinaryOperations.ByteConverter.combineByteArrays(messageHeader, messageBody);
+            return fullMessage.buffer;
+        }
+    }
 }
