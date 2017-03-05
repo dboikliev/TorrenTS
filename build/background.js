18e5450a61e76f006ed074e0ce07ed9a400230d4
dboikliev <deyanpb@hotmail.com>
2017-03-05 16:47:10 +0200

    Removes tracking of build files.

--- build/background.js
+++
@@ -1,9 +0,0 @@
-// chrome.app.runtime.onLaunched.addListener(function() {
-//   chrome.app.window.create("../../html/index.html", {
-//     "outerBounds": {
-//       "width": 400,
-//       "height": 500
-//     },
-//   });
-// });
-//# sourceMappingURL=background.js.map
